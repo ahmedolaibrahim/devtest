199a6af2ab1d3d3e6fc5ca778f3c77e12d2fcb97
Ibrahim ahmed <Ibrahim ahmed>
2016-10-18 23:42:55 +0100

    dev test commit

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+/**
+ * Main application file
+**/
+
+// Setup server
 var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+
+//serve up the index.html file 
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
+// Start up the server on either port 3000 or detected environment port
 app.listen(process.env.PORT || 3000 , function () {
   console.log('devtest App Server - listening on port: ' + process.env.PORT || 3000);
 });
